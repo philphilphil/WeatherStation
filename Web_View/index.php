707c241027a17bdc090033de616d1256292e8aa5
Philipp Baum <phil@phib.io>
2016-11-02 21:24:09 +0100

    Update Webview, only indoor for now

--- Web_View/index.php
+++ Web_View/index.php
@@ -9,8 +9,9 @@
 <?php
 
 $str = file_get_contents('/var/www/phib.io/weather/temps.json');
-$json = json_decode($str, true);
-
+#Add [ ] and remove last , for valid json
+$strFixed = "[".substr($str,0, -1)."]";
+$json = json_decode($strFixed, true);
 
 #print_r(json_encode(array_column($json, 'date')));
 #print_r(json_encode(array_column($json, 'temp')));
@@ -20,18 +21,18 @@ $json = json_decode($str, true);
 <table cellpadding="50" border="0">
 	<tr>
 		<td><h1>Indoor: <?php echo($json[count($json)-1]["temp"]); ?>C&deg;</h1><h6>measured at <?php echo($json[count($json)-1]["date"]); ?></h6></td>
-		<td><h1>Outdoor: <?php echo($json[count($json)-1]["temp"]); ?>C&deg;</h1><h6>measured at <?php echo($json[count($json)-1]["date"]); ?></h6></td>
+		<td><h1>Outdoor: <?php echo("tbd");#echo($json[count($json)-1]["temp"]);  ?>C&deg;</h1><h6>measured at <?php echo($json[count($json)-1]["date"]); ?></h6></td>
 	</tr>
 </table>
 
+<h4><b>Last 24h</b></h4>
 <div class="ct-chart ct-perfect-fourth" style="height: 510px; margin: auto; width: 50%">
-<h4><b>Last 24h, measured every 30 min</b></h4>
 <script type="text/javascript">
 	var data = {
   	labels: <?php print_r(json_encode(array_column($json, 'date'))); ?>,
   	series: [
-			<?php print_r(json_encode(array_column($json, 'temp'))); ?>,
-    		[11, 13, 21, 31, 10, 13, 11]
+			<?php print_r(json_encode(array_column($json, 'temp'))); ?>
+    
   			]
 	};
 
@@ -40,7 +41,7 @@ $json = json_decode($str, true);
   		height: 500
 		};
 new Chartist.Line('.ct-chart', data, options);</script></div>
-<h6>Arduino-powered Weather station with 3D-printed enclosure - Code on <a href='https://github.com/philphilphil/WeatherStation'>GitHub</a> - Blogpost about the hardware inc.</h6>
+<h6>Arduino-powered Weather station with 3D-printed enclosure - Code on <a href='https://github.com/philphilphil/WeatherStation'>GitHub</a> - Blogpost about the hardware</h6>
 </center>
 </body>
 </html>
