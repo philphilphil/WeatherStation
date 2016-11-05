590ae41742eb25ebc3f183d040beed43949b21e7
Philipp Baum <phil@phib.io>
2016-11-05 14:15:38 +0100

    added second sensor to write

--- Server/write_temp.php
+++ Server/write_temp.php
@@ -1,16 +1,20 @@
 <?php
 
-if (!isset($_GET["temp"]) || !isset($_GET["key"]))
+if (!isset($_GET["tempi"]) || !isset($_GET["tempo"]) || !isset($_GET["humo"]) || !isset($_GET["key"]))
 {
 exit("nope");
 }
 
-$data["date"] = date('d-m-Y H:i');
-$data["temp"] = $_GET["temp"];
-
 if($_GET["key"] != "secretKey") {
 	exit("nope");
 }
+
+$data["date"] = date('d-m-Y H:i');
+$data["tempi"] = $_GET["tempi"];
+$data["tempo"] = $_GET["tempo"];
+$data["humo"] = $_GET["humo"];
+
+
 
 $fp = fopen('/var/www/phib.io/weather/temps.json', 'a');
 fwrite($fp, json_encode($data).",");
